8ede8a6407539bbd19180729a9ceb78e1ede6403
Esther-SB <esther.sanchob@gmail.com>
2025-05-06 18:58:15 +0200

    Seguimos

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String titulus;
+  final String subtitulus;
+  final String link;
+  final IconData icon;
+
+
+const MenuItem({
+  required this.titulus,
+  required this.subtitulus,
+  required this.link,
+  required this.icon,
+})
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:master_ifab/config/config.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,12 +8,22 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Yemart'),
+@override
+  Widget build(BuildContext context) { //el metodo build, devuelve un widget (objeto visual con su funcion)
+    //buildcontext es toda la info del android
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //esto para quitar la etiquetado de construccion
+      theme: AppTheme(electusColor: 1).getTheme(),
+      home: Scaffold( //widget que ocupa toda la pantalla
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: const Color.fromARGB(255, 61, 160, 241)
+      ),
+        body: Center( //center es que todo lo que tenga dentro de body, va centrado automáticamente
+          child:FilledButton(
+            onPressed: () {}, 
+            child: Text('Esbosan')
+            )
         ),
       ),
     );
