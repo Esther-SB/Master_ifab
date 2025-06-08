72c548256c21db8043424b204ee4264636eeb467
Esther-SB <esther.sanchob@gmail.com>
2025-06-08 11:46:36 +0200

    state notifier provider

--- lib/config/entities/bands.dart
+++ lib/config/entities/bands.dart
@@ -8,4 +8,31 @@ class Band {
     required this.nomen,
     required this.numerusVotum,
   });
+
+factory Band.fromMap(Map<String, dynamic> obj) => Band(
+    id: obj['id'] as String,
+    nomen: obj['nomen'] as String,
+    numerusVotum: obj['numerusVotum'] as int,
+);
+
+Band copyWith({
+    String? id,
+    String? nomen,
+    int? numerusVotum,
+  }) {
+    return Band(
+      id: id ?? this.id,
+      nomen: nomen ?? this.nomen,
+      numerusVotum: numerusVotum ?? this.numerusVotum,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nomen': nomen,
+      'numerusVotum': numerusVotum,
+    };
+  }
+
 }
